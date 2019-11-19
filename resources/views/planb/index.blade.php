6f08665255440073df41ecd4fcf7100cc6e12399
faturprayuda <faturprayuda@gmail.com>
2019-11-20 01:47:30 +0700

    make form 2nd layout complete

--- resources/views/planb/index.blade.php
+++ resources/views/planb/index.blade.php
@@ -14,22 +14,25 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/planb/style.css') }}">
 
     {{-- templateCSS --}}
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset("vendor/bootstrap/css/bootstrap.min.css") }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset("vendor/animate/animate.css") }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset("vendor/css-hamburgers/hamburgers.min.css") }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset("vendor/animsition/css/animsition.min.css") }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset("vendor/select2/select2.min.css") }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset("vendor/daterangepicker/daterangepicker.css") }}">
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/planb/util.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/planb/main.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('iconic/css/material-design-iconic-font.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <!--===============================================================================================-->
 
     <title>Hello, world!</title>
@@ -39,33 +42,66 @@
     <div class="container">
         <div class="row">
             <div class="col-5">
-                {{-- Logo --}}
-                <img src="{{asset('img/logo.jpeg')}}" alt="">
-                {{-- End Logo --}}
+                {{-- header --}}
+                <img src="{{asset('img/logo.jpeg')}}" alt="logo" class="logo">
+                <div class="font-header">
+                    <h3>Login</h3>
+                    <h5>Silahkan isi form.</h5>
+                </div>
+                {{-- End header --}}
 
                 {{-- form --}}
-                <div class="container form">
-                    <div class="wrap-input100 validate-input" data-validate="Valid email is: a@b.c">
-                        <input class="input100" type="text" name="id">
-                        <span class="focus-input100" data-placeholder="ID SISWA"></span>
+                <form action="#">
+                    <div class="form">
+                        <div class="wrap-input100 validate-input" data-validate="Valid email is: a@b.c">
+                            <input class="input100" type="text" name="id">
+                            <span class="focus-input100" data-placeholder="ID SISWA"></span>
+                        </div>
+                        <div class="wrap-input100 validate-input" data-validate="Enter password">
+                            <span class="btn-show-pass">
+                                <i class="zmdi zmdi-eye"></i>
+                            </span>
+                            <input class="input100" type="password" name="pass">
+                            <span class="focus-input100" data-placeholder="Password"></span>
+                        </div>
                     </div>
-                    <div class="wrap-input100 validate-input" data-validate="Enter password">
-                        <span class="btn-show-pass">
-                            <i class="zmdi zmdi-eye"></i>
-                        </span>
-                        <input class="input100" type="password" name="pass">
-                        <span class="focus-input100" data-placeholder="Password"></span>
+
+                    <div class="forgot-pass">
+                        <h5><a href="#">Lupa Password?</a></h5>
                     </div>
-                </div>
+
+                    <div class="container-login100-form-btn">
+                        <div class="wrap-login100-form-btn">
+                            <div class="login100-form-bgbtn"></div>
+                            <button class="login100-form-btn">
+                                Hadir
+                            </button>
+                        </div>
+                    </div>
+                </form>
                 {{-- End Form --}}
+
+                {{-- Footer --}}
+                <h5 class="footer">Copyright Reserved 2019 | SMP 128</h5>
+                {{-- End Footer --}}
             </div>
 
-            {{-- Image --}}
             <div class="col-5">
+                {{-- Image --}}
                 <img src="https://image.freepik.com/free-vector/happy-children-school_23-2147532720.jpg" alt="img"
-                    class="logo">
+                    class="img">
+                {{-- End Image --}}
+
+                <div class="container-banner">
+                    <div class="container">
+                        <div class="container">
+                            <h2 class="welcome">Selamat Datang</h2>
+                            <h6 class="line"></h6>
+                            <h2 class="slogan">SIAT SMP Negeri 128 Depok</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {{-- End Image --}}
         </div>
     </div>
 
@@ -83,21 +119,21 @@
 
     {{-- scriptJS Template --}}
     <!--===============================================================================================-->
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <script src="{{ asset('vendor/animsition/js/animsition.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/bootstrap/js/popper.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ asset('vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/select2/select2.min.js"></script>
+    <script src="{{ asset('vendor/select2/select2.min.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/daterangepicker/moment.min.js"></script>
-    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <script src="{{ asset('vendor/daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('vendor/daterangepicker/daterangepicker.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <script src="{{ asset('vendor/countdowntime/countdowntime.js') }}"></script>
     <!--===============================================================================================-->
-    <script src="js/main.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
 
 </html>
